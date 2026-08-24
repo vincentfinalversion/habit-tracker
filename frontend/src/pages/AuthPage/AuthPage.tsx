9ab56d9c59c7004ee5cfc10919e2a6eaf5bc52a2
vincent <vincent.work.exe@gmail.com>
2026-08-24 14:38:42 +0800

    feat: AuthPage toggle buttons correct state

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,39 @@
+import Header from '../../components/Header/Header.tsx';
+import Button from '../../components/Button/Button.tsx';
+import { useState } from 'react';
+import './AuthPage.css';
+
 function AuthPage() {
+  const [activeTab, setActiveTab] = useState<'login' | 'register'>('login');
+
   return (
     <div>
-      <h1>Auth Page</h1>
+      <main className="auth-main">
+        <div className="auth-info-container">
+          <h2 className="auth-info-title">
+            Stay consistent.
+          </h2>
+          <p className="auth-info-description">
+            Build habits that stick, one day at a time.
+          </p>
+        </div>
+        <div className="auth-form-container">
+          <div className="toggle-buttons-container">
+            <Button
+              className={activeTab === 'login' ? 'active' : ''}
+              onClick={() => setActiveTab('login')}
+            > 
+              Login
+            </Button>
+            <Button
+              className={activeTab === 'register' ? 'active' : ''}
+              onClick={() => setActiveTab('register')}
+            > 
+              Register
+            </Button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
