72dc1cc488e783478792aec07acd649d337b699e
vincent <vincent.work.exe@gmail.com>
2026-08-25 02:23:05 +0800

    feat: add resusable TextField component

---
+++ frontend/src/components/TextField/TextField.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react";
+import './TextField.css';
+
+type TextFieldProps = {
+  label?: string;
+  error?: string;
+} & ComponentProps<"input">;
+
+function TextField({ 
+  placeholder,
+  label,
+  error, 
+  ...props 
+}: TextFieldProps) {
+  return (
+    <div className="text-field">
+      {label && <label className="text-field-label">{label}</label>}
+      <input 
+        placeholder={placeholder}
+        className="text-field-input"
+        {...props} 
+      />
+      {error && (
+        <span className="text-field-error">
+          {error}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default TextField;
