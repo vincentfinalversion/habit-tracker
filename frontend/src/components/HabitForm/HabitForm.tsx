2369323e14da5d82ad61bba0a99b53c20d8f94df
vincent <vincent.work.exe@gmail.com>
2026-08-25 02:23:40 +0800

    feat: update HabitForm component to use TextField

--- frontend/src/components/HabitForm/HabitForm.tsx
+++ frontend/src/components/HabitForm/HabitForm.tsx
@@ -1,29 +1,29 @@
 import { useState, type SubmitEvent } from "react";
-import Button from "../Button/Button.tsx";
 import { useHabits } from "../../context/HabitProvider.tsx";
+import Button from "../Button/Button.tsx";
+import TextField from "../TextField/TextField.tsx";
 import "./HabitForm.css";
 
 function HabitForm() {
-	const [name, setName] = useState("");
+	const [habitName, setHabitName] = useState("");
 	const { addHabit } = useHabits();
 
 	function handleSubmit(e: SubmitEvent) {
 		e.preventDefault();
 
-		if (name.trim() === "") return;
-		setName("");
-		addHabit(name);
+		if (habitName.trim() === "") return;
+		setHabitName("");
+		addHabit(habitName);
 	}
 
 	return (
 		<form className="habit-form" onSubmit={handleSubmit}>
-			<input
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-				className="habit-form-input"
-				placeholder="New Habit"
-			/>
-			<Button disabled={name.trim() === ""} className="habit-form-button">
+      <TextField 
+        placeholder="Enter Habit Name..."
+        value={habitName}
+        onChange={(e) => setHabitName(e.target.value)}  
+      />
+			<Button disabled={habitName.trim() === ""} className="habit-form-button">
 				Add Habit
 			</Button>
 		</form>
