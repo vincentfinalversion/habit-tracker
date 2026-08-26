a8270ecfd795c4696e625ca9bf146838a90eb7e4
vincent <vincent.work.exe@gmail.com>
2026-08-26 14:31:14 +0800

    refactor: AuthPage styles

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -9,33 +9,31 @@ function AuthPage() {
 
   return (
     <div className="auth-page">
-      <main className="auth-main">
-        <div className="auth-info-container">
-          <h2 className="auth-info-title">
-            myHabits
-          </h2>
-          <p className="auth-info-description">
-            Stay consistent. Build habits that stick, one day at a time.
-          </p>
+      <div className="auth-info-container">
+        <h2 className="auth-info-title">
+          myHabits
+        </h2>
+        <p className="auth-info-description">
+          Stay consistent. Build habits that stick, one day at a time.
+        </p>
+      </div>
+      <div className="auth-form-container">
+        <div className="toggle-buttons-container">
+          <Button
+            className={activeTab === 'login' ? 'active' : ''}
+            onClick={() => setActiveTab('login')}
+          > 
+            Login
+          </Button>
+          <Button
+            className={activeTab === 'register' ? 'active' : ''}
+            onClick={() => setActiveTab('register')}
+          > 
+            Register
+          </Button>
         </div>
-        <div className="auth-form-container">
-          <div className="toggle-buttons-container">
-            <Button
-              className={activeTab === 'login' ? 'active' : ''}
-              onClick={() => setActiveTab('login')}
-            > 
-              Login
-            </Button>
-            <Button
-              className={activeTab === 'register' ? 'active' : ''}
-              onClick={() => setActiveTab('register')}
-            > 
-              Register
-            </Button>
-          </div>
-          {activeTab === 'login' ? <LoginForm /> : <RegisterForm />}
-        </div>
-      </main>
+        {activeTab === 'login' ? <LoginForm /> : <RegisterForm />}
+      </div>
     </div>
   );
 };
