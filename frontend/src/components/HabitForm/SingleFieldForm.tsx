73f5d1a3729272fcdb6d6ce6462287253d0293fe
vincent <vincent.work.exe@gmail.com>
2026-08-28 13:37:48 +0800

    refactor: GenericForm into SingleFieldForm for readability

---
+++ frontend/src/components/HabitForm/SingleFieldForm.tsx
@@ -0,0 +1,42 @@
+import type { SubmitEventHandler, ComponentProps } from "react";
+import Button from "../Button/Button.tsx";
+import TextField from "../TextField/TextField.tsx";
+import "./SingleFieldForm.css";
+
+type SingleFieldFormProps = {
+  onSubmit: SubmitEventHandler<HTMLFormElement>;
+  buttonText: string;
+  buttonDisabled?: boolean;
+  className?: string;
+} & Pick<ComponentProps<typeof TextField>, "label" | "error"> &
+  ComponentProps<"input">;
+
+function SingleFieldForm({
+  onSubmit,
+  buttonText,
+  buttonDisabled = false,
+  className = "",
+  label,
+  error,
+  ...inputProps
+}: SingleFieldFormProps) {
+  return (
+    <form className={`single-field-form ${className}`} onSubmit={onSubmit}>
+      <TextField
+        label={label}
+        error={error}
+        {...inputProps}
+      />
+      <div className="button-container">
+        <Button
+          disabled={buttonDisabled}
+          className="single-field-form-button"
+        >
+          {buttonText}
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default SingleFieldForm;
