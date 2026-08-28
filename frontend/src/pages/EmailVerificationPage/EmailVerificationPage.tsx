dd917d66766451c5e8b958b5a7dcb97ae0581d98
vincent <vincent.work.exe@gmail.com>
2026-08-28 13:39:01 +0800

    feat: EmailVerification page layout

--- frontend/src/pages/EmailVerificationPage/EmailVerificationPage.tsx
+++ frontend/src/pages/EmailVerificationPage/EmailVerificationPage.tsx
@@ -1,9 +1,32 @@
+import { type SubmitEvent } from 'react';
+import SingleFieldForm from '../../components/HabitForm/SingleFieldForm';
+import './EmailVerificationPage.css';
+
 function EmailVerificationPage() {
+  const email = 'example@email.com';
+
+  const handleSubmit = (
+    e: SubmitEvent<HTMLFormElement | HTMLInputElement>,
+  ) => {
+    e.preventDefault();
+    
+    // TODO: Verify the code
+  };
+
   return (
-    <div>
-      <h1>Email Verification Page</h1>
+    <div className="email-verification-page">
+      <SingleFieldForm
+        // label={`6 digit code sent to ${email}`}
+        placeholder="Enter the code"
+        type="text"
+        inputMode="numeric"
+        maxLength={6}
+        required
+        buttonText="Verify"
+        onSubmit={handleSubmit}
+      />
     </div>
   );
-};
+}
 
 export default EmailVerificationPage;
